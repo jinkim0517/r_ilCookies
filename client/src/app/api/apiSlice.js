ada807fb9ac3f29bab271d6be8a02adfd47a462d
jinkim0517 <jinsungkim2004@gmail.com>
2024-04-28 13:08:16 -0700

    updated endpoint URL

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://r-ilcookies-api.onrender.com' }),
     tagTypes: ['Cookie'],
     endpoints: builder => ({})
 })
